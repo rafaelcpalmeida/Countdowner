6e2d26e997a0d0a9bfbda458844eed2b9b697ae0
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-09-07 00:59:59 +0100

    Implement skeleton application

---
+++ Countdowner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Countdowner
+//
+//  Created by Rafael Almeida on 06/09/17.
+//  Copyright © 2017 Rafael Almeida. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Countdowner/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Countdowner
+//
+//  Created by Rafael Almeida on 06/09/17.
+//  Copyright © 2017 Rafael Almeida. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+    @IBOutlet weak var countDownLabel: NSTextField!
+    
+    var addedObserver: Bool = false
+    var counter: Int = 1800
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let window = self.view.window {
+            window.level = Int(CGWindowLevelForKey(CGWindowLevelKey.floatingWindow))
+        } else {
+            addedObserver = true
+            self.addObserver(self, forKeyPath: "view.window", options: [.new, .initial], context: nil)
+        }
+        
+        _ = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+    }
+    
+    override open func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if let window = self.view.window {
+            window.level = Int(CGWindowLevelForKey(CGWindowLevelKey.floatingWindow))
+        }
+    }
+    
+    deinit {
+        if addedObserver {
+            self.removeObserver(self, forKeyPath: "view.window")
+        }
+    }
+    
+    func update() {
+        if(counter > 0) {
+            let minutes = String(counter / 60)
+            let seconds = String(counter % 60)
+            
+            self.countDownLabel.stringValue = String(describing: minutes + ":" + seconds)
+            
+            counter -= 1
+        }
+    }
+}
