86bb35a52c63e1eb897e6aa776007ff7339382ad
Rafael Almeida <rafaelcpalmeida@gmail.com>
2018-01-29 11:59:12 +0000

    Create iOS basic structure

---
+++ Countdowner iOS/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Countdowner iOS
+//
+//  Created by Rafael Almeida on 29/01/18.
+//  Copyright © 2018 Rafael Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
