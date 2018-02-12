fe299ce5fa2a4642096703d6d5b3f36a41dd4e44
Rafael Almeida <rafaelcpalmeida@gmail.com>
2018-02-12 21:01:09 +0000

    Remove unused code

--- Countdowner iOS/Controller/ViewController.swift
+++ Countdowner iOS/Controller/ViewController.swift
@@ -23,23 +23,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*// Create a DatePicker
-        let datePicker: UIDatePicker = UIDatePicker()
-        
-        // Posiiton date picket within a view
-        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200)
-        
-        // Set some of UIDatePicker properties
-        datePicker.timeZone = NSTimeZone.local
-        datePicker.backgroundColor = UIColor.white
-        datePicker.datePickerMode = .countDownTimer
-        
-        // Add an event to call onDidChangeDate function when value is changed.
-        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: .valueChanged)
-        
-        // Add DataPicker to the view
-        self.view.addSubview(datePicker)*/
-        
         countdownerService.delegate = self
         
         self.setDefaultCounterValue()
@@ -49,20 +32,6 @@ class ViewController: UIViewController {
         self.view.layer.backgroundColor = CGColor.green
             
         UIApplication.shared.isIdleTimerDisabled = true
-    }
-    
-    @objc func datePickerValueChanged(_ sender: UIDatePicker){
-        
-        // Create date formatter
-        let dateFormatter: DateFormatter = DateFormatter()
-        
-        // Set date format
-        dateFormatter.dateFormat = "mm:ss"
-        
-        // Apply date format
-        let selectedDate: String = dateFormatter.string(from: sender.date)
-        
-        print("Selected value \(selectedDate)")
     }
 
     override func didReceiveMemoryWarning() {
