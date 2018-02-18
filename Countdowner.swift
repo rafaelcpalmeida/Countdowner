4ca8ea50d0f160970855fd391b14656f906c2e97
Rafael Almeida <rafaelcpalmeida@gmail.com>
2018-02-18 16:16:41 +0000

    Fix Codacy code style issues

--- Countdowner.swift
+++ Countdowner.swift
@@ -28,7 +28,7 @@ class Countdowner {
     func update(counter: Int) -> (window: WindowSettings, color: CGColor, minutes: Int, seconds: Int) {
         let minutes = counter / 60
         let seconds = counter % 60
-        
+
         switch Double(counter) {
         case self.danger ... self.alert:
             color = .yellow
@@ -39,24 +39,24 @@ class Countdowner {
         default:
             return self.defaultState(counter: counter)
         }
-        
+
         return (window: window!, color: color!, minutes: minutes, seconds: seconds)
     }
-    
+
     func secondsToTime(seconds: Int) -> (timeInMinutes: Int, timeInSeconds: Int) {
         return (timeInMinutes: seconds / 60, timeInSeconds: seconds % 60)
     }
-    
+
     func defaultState(counter: Int) -> (window: WindowSettings, color: CGColor, minutes: Int, seconds: Int) {
         let minutes = counter / 60
         let seconds = counter % 60
-        
+
         color = .green
         window = WindowSettings(width: 200, height: 100, x: 25, y: 25)
-        
+
         return (window: window!, color: color!, minutes: minutes, seconds: seconds)
     }
-    
+
     func setCountdownValue(counter: Int) {
         self.counter = counter
         self.alert = Double(counter) * 0.33
--- Countdowner/Controller/ViewController.swift
+++ Countdowner/Controller/ViewController.swift
@@ -25,14 +25,14 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.countdownerService.delegate = self
-        
+
         self.setDefaultCounterValue()
         self.countdowner = Countdowner(counter: counter)
         self.setTime()
         self.view.layer?.backgroundColor = .green
-        
+
         if let window = self.view.window {
             window.level = NSWindow.Level(rawValue: Int(CGWindowLevelForKey(.floatingWindow)))
         } else {
@@ -64,16 +64,16 @@ class ViewController: NSViewController {
     @IBAction func settingsButton(_ sender: NSButton) {
         let alert = NSAlert()
         let secondsField = NSTextField(frame: NSRect(x: 0, y: 0, width: 300, height: 24))
-        
+
         secondsField.placeholderString = NSLocalizedString("30 minutes equals to 1800 seconds", comment: "")
         secondsField.formatter = OnlyIntegerValueFormatter()
-        
+
         alert.messageText = NSLocalizedString("Please insert the value, in seconds, of the timer:", comment: "")
         alert.alertStyle = .informational
         alert.addButton(withTitle: NSLocalizedString("OK", comment: ""))
         alert.addButton(withTitle: NSLocalizedString("Cancel", comment: ""))
         alert.accessoryView = secondsField
-        
+
         if alert.runModal() == .alertFirstButtonReturn,
             let seconds = Int(secondsField.stringValue),
             seconds > 0 {
@@ -86,7 +86,7 @@ class ViewController: NSViewController {
 
     func setTime() {
         guard let countdownerDetails = self.countdowner?.secondsToTime(seconds: counter) else { fatalError() }
-        
+
         self.countDownLabel.stringValue = String(describing: "\(String(format: "%02d", countdownerDetails.timeInMinutes)):\(String(format: "%02d", countdownerDetails.timeInSeconds))")
     }
 
@@ -115,28 +115,28 @@ class ViewController: NSViewController {
 
     func pauseTimer() {
         self.countdownerService.send(action: .pause)
-        
+
         self.countdownTimer?.invalidate()
     }
 
     func resetTimer() {
         self.countdownerService.send(action: .reset)
-        
+
         self.handleTimer()
-        
+
         self.setDefaultCounterValue()
-        
+
         let countdownerDetails = self.countdowner!.defaultState(counter: counter)
-        
+
         self.updateWindow(color: countdownerDetails.color, width: countdownerDetails.window.width, height: countdownerDetails.window.height, x: countdownerDetails.window.x, y: countdownerDetails.window.y, minutes: countdownerDetails.minutes, seconds: countdownerDetails.seconds)
     }
 
     @objc func update() {
         if counter > 0 {
             counter -= 1
-            
+
             guard let countdownerDetails = self.countdowner?.update(counter: counter) else { fatalError() }
-            
+
             self.updateWindow(color: countdownerDetails.color, width: countdownerDetails.window.width, height: countdownerDetails.window.height, x: countdownerDetails.window.x, y: countdownerDetails.window.y, minutes: countdownerDetails.minutes, seconds: countdownerDetails.seconds)
         } else {
             self.pauseTimer()
