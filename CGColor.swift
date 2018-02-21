12bdaf55c0ffd4a106885d8447c22b4e576ef1ac
Rafael Almeida <rafaelcpalmeida@gmail.com>
2018-02-21 22:35:58 +0000

    Remove duplicated code

--- CGColor.swift
+++ CGColor.swift
@@ -9,17 +9,15 @@
 #if os(iOS) || os(watchOS) || os(tvOS)
     import UIKit
 
-    extension CGColor {
-        static var green = UIColor(red:0.30, green:0.69, blue:0.31, alpha:1.0).cgColor
-        static var yellow = UIColor(red:1.00, green:0.92, blue:0.23, alpha:1.0).cgColor
-        static var red = UIColor(red:0.96, green:0.26, blue:0.21, alpha:1.0).cgColor
-    }
+    typealias Color = UIColor
 #elseif os(OSX)
     import Cocoa
 
-    extension CGColor {
-        static var green = NSColor(red:0.30, green:0.69, blue:0.31, alpha:1.0).cgColor
-        static var yellow = NSColor(red:1.00, green:0.92, blue:0.23, alpha:1.0).cgColor
-        static var red = NSColor(red:0.96, green:0.26, blue:0.21, alpha:1.0).cgColor
-    }
+    typealias Color = NSColor
 #endif
+
+extension CGColor {
+    static var green = Color(red:0.30, green:0.69, blue:0.31, alpha:1.0).cgColor
+    static var yellow = Color(red:1.00, green:0.92, blue:0.23, alpha:1.0).cgColor
+    static var red = Color(red:0.96, green:0.26, blue:0.21, alpha:1.0).cgColor
+}
